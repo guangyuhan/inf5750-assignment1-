d5a3f9a5b72f67009280e878670e759d96ca8c41
guangyuhan <lightupdreams@gmail.com>
2014-09-19 20:13:25 +0200

    heh

--- assignment1_guangyuh/src/main/java/no/uio/inf5750/assignment1_guangyuh/dao/MessageDao.java
+++
@@ -1,9 +0,0 @@
-
-package no.uio.inf5750.assignment1_guangyuh.dao;
-import no.uio.inf5750.assignment1_guangyuh.model.Message;
-public interface MessageDao
-{
-    int save( Message message );
-    Message get( int id );
-    Message getLast();    
-}
